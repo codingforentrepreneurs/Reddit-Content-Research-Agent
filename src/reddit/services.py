94fbe18e76fff91f6664f930b2fc6a12a0f21ed0
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-09-09 12:55:34 -0600

    47 - Scraping Workflow as a Service Function

--- src/reddit/services.py
+++ src/reddit/services.py
@@ -1,6 +1,11 @@
 from functools import lru_cache
+from datetime import timedelta
+import helpers.bd
+from django.utils import timezone
 
-from .models import RedditPost
+from snapshots.tasks import perform_reddit_scrape_task
+
+from .models import RedditPost, RedditCommunity
 
 @lru_cache
 def get_valid_reddit_post_fields():
@@ -26,3 +31,51 @@ def handle_reddit_thread_results(reddit_results:list = []):
         )
         ids.append(instance.id)
     return ids
+
+
+
+
+def handle_reddit_community_scrape_automation(instance, created=False, force_scrape=False, verbose=False):
+    # print(instance.pk, instance.url)
+    instance.refresh_from_db()
+    url = instance.url
+    active = instance.active 
+    if not active and not force_scrape:
+        return
+    now = timezone.now()
+    last_scrape_event = instance.last_scrape_event
+    last_event_delta = None
+    min_last_event_delta = timedelta(minutes=5)
+    if last_scrape_event is not None:
+        last_event_delta = now - last_scrape_event
+        scrape_ready = last_event_delta > min_last_event_delta
+    else:
+        scrape_ready = True
+    if force_scrape:
+        scrape_ready = True
+    if verbose:
+        print("Ready to scrape",scrape_ready, instance.url)
+        print("Was jsut created", created)
+    qs = RedditCommunity.objects.filter(pk=instance.pk)
+    qs.update(last_scrape_event = timezone.now())
+    if scrape_ready and not created:
+        if verbose:
+            print("Trigger reddit community post scrape update")
+        perform_reddit_scrape_task.delay(
+            url, 
+            num_of_posts = 5, 
+            progress_countdown=300,
+            sort_by_time = "This Week"
+        )
+    if created:
+        if verbose:
+            print("First pass reddit community run")
+        for sort_by in helpers.bd.BRIGHT_DATA_SCRAPE_SORT_OPTIONS:
+            perform_reddit_scrape_task.delay(
+                url, 
+                num_of_posts = 5, 
+                progress_countdown=300,
+                sort_by_time=sort_by
+            )
+    if verbose:
+        print("---done---\n")
--- src/reddit/signals.py
+++ src/reddit/signals.py
@@ -1,17 +1,16 @@
-from datetime import timedelta
-import helpers.bd
 from django.db.models.signals import post_save
-from django.utils import timezone
-
-from snapshots.tasks import perform_reddit_scrape_task
 
 from .models import RedditCommunity
-from . import services
+
+from . import services as reddit_db_services
 
 
 def reddit_community_post_save_receiver(sender, instance, created, *args, **kwargs):
-    # print(instance.pk, instance.url)
-    services.handle_reddit_community_scraping(instance, created)
-    
+    reddit_db_services.handle_reddit_community_scrape_automation(
+            instance, 
+            created=created, 
+            force_scrape=False,
+            verbose=True
+    )
 
 post_save.connect(reddit_community_post_save_receiver, sender=RedditCommunity)
